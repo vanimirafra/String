ecc9181099efbe8e56eba22bf1a7373f23204fbb
vanimirafra <53507455+vanimirafra@users.noreply.github.com>
2020-08-27 23:50:26 +0530

    Create str.py

---
+++ str.py
@@ -0,0 +1,18 @@
+'''Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead of the empty string. Go to the editor
+Sample String : 'w3resource'
+Expected Result : 'w3ce'
+Sample String : 'w3'
+Expected Result : 'w3w3'
+Sample String : ' w'
+Expected Result : Empty String'''
+
+def string_both_ends(str):
+  if len(str) < 2:
+    return ''
+  else:
+    return str[0:2] + str[-2:]
+
+print(string_both_ends('w3resource'))
+print(string_both_ends('w3'))
+print(string_both_ends('w'))
+
